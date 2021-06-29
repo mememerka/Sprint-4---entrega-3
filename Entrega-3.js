f22027695a3e6912f49bff588567b33bf77a3d18
Oriol Mercader <oriol.mercader@gmail.com>
2021-06-29 10:28:14 +0200

    Exercici2

--- Entrega-3.js
+++ Entrega-3.js
@@ -53,3 +53,16 @@ let power = (base, exponent) => {
  }*/
 
  let greet = who => console.log("Hello "+ who);
+
+ //EXERCICI 2
+
+var users = 
+[{ firstName: 'Homer', lastName: 'Simpson' },
+{ firstName: 'Marge', lastName: 'Simpson' },
+{ firstName: 'Bart', lastName: 'Simpson' },
+{ firstName: 'Lisa', lastName: 'Simpson' },
+{ firstName: 'Maggie', lastName: 'Simpson' }];
+
+users.map(function(user) {
+    user.firstName;
+ });
